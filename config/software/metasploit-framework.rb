efc5808045839bba007caca33a889d6423f71d3c
Brent Cook <bcook@rapid7.com>
2015-02-26 14:06:07 -0600

    flesh out actually packaging framework, add bin stubs
    Now, on install, you can run /opt/metasploit/bin/msfconsole and have it
    run as expected :)

--- config/software/metasploit-framework.rb
+++ config/software/metasploit-framework.rb
@@ -1,7 +1,11 @@
 name "metasploit-framework"
+default_version "master"
+#source git: "https://github.com/rapid7/metasploit-framework.git"
+
 default_version "4.11.1"
-source url: "https://github.com/rapid7/metasploit-framework/archive/4.11.1.tar.gz",
+source url: "https://github.com/rapid7/metasploit-framework/archive/#{default_version}.tar.gz",
        md5: "8194917a8b5e53f9f72e8ba55df8c8cf"
+relative_path "metasploit-framework-#{default_version}"
 
 dependency "bundler"
 dependency "liblzma"
@@ -10,8 +14,41 @@ dependency "ruby"
 dependency "postgresql"
 dependency "sqlite"
 
-relative_path "metasploit-framework-4.11.1"
+whitelist_file "#{install_dir}//framework/data/isight.bundle"
 
 build do
+	command "mkdir #{install_dir}/bin"
+
+	metasploit_bins = [
+	      'msfbinscan',
+	      'msfcli',
+	      'msfconsole',
+	      'msfd',
+	      'msfelfscan',
+	      'msfencode',
+	      'msfmachscan',
+	      'msfpayload',
+	      'msfpescan',
+	      'msfrop',
+	      'msfrpc',
+	      'msfrpcd',
+	      'msfupdate',
+	      'msfvenom'
+	]
+
+	metasploit_bins.each { |bin|
+		command "cat << EOF > #{install_dir}/bin/#{bin}
+cd #{install_dir}/framework
+#{install_dir}/embedded/bin/bundle exec \
+#{install_dir}/embedded/bin/ruby \
+#{install_dir}/framework/#{bin}
+cd -
+EOF"
+	}
+
+	command "chmod +x #{install_dir}/bin/msfconsole"
+
+	copy "#{project_dir}", "#{install_dir}/framework"
+
 	bundle "install"
 end
