54704613bb12d7f4deade1c5828cbcf5cb1fd638
Brent Cook <bcook@rapid7.com>
2015-05-22 16:37:31 -0500

    update john to 1.8.0 jumbo, do not link openmp

--- config/software/john.rb
+++ config/software/john.rb
@@ -1,16 +1,19 @@
 name "john"
-default_version "1.8.0"
+default_version "1.8.0-jumbo-1"
 
-version("1.8.0") { source md5: "a4086df68f51778782777e60407f1869" }
+version("1.8.0-jumbo-1") { source md5: "1d6b22ec41a12cdcd62ad6eae3e77345" }
 
-source url: "http://www.openwall.com/john/j/john-1.8.0.tar.xz"
+source url: "http://www.openwall.com/john/j/john-1.8.0-jumbo-1.tar.xz"
 
 relative_path "john-#{version}/src"
 
+dependency "openssl"
+
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  command "./configure --prefix=#{install_dir}/embedded --disable-openmp", env: env
   make env: env
-  make "clean generic", env: env
-  command "chmod +r #{project_dir}/../run/john.conf"
-  copy "#{project_dir}/../run/*", "#{install_dir}/embedded/bin/"
+  make "install", env: env
+  mkdir "#{install_dir}/embedded/john"
+  copy "#{project_dir}/../run/*", "#{install_dir}/embedded/john/"
 end
