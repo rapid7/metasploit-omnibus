fc1a0b4a8e9f0d52f0b1007d11e2369537eea72f
Brent Cook <bcook@rapid7.com>
2015-08-28 13:15:53 -0500

    move maintainer to Rapid7 Release Engineering

--- config/projects/metasploit-framework-extras.rb
+++ config/projects/metasploit-framework-extras.rb
@@ -1,5 +1,5 @@
 name "metasploit-framework-extras"
-maintainer "Brent Cook <bcook@rapid7.com>"
+maintainer "Rapid7 Release Engineering <r7_re@rapid7.com>"
 homepage "https://rapid7.com"
 
 install_dir "#{default_root}/metasploit-framework-extras"
