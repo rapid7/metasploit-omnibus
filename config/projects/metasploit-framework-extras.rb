d1a34dcce98e11794addbbd2bd2e55f60419e2e9
wfvwhwwh <40670933+wfvwhwwh@users.noreply.github.com>
2018-06-28 17:14:37 +0000

    Remove extras from the installer
    John and Nmap are not necessary, and for users that use John or Nmap from source or just does not use theses tools, its only more downloading and compiling time, and conflicts
    
    theses are not true dependencies

--- config/projects/metasploit-framework-extras.rb
+++ config/projects/metasploit-framework-extras.rb
@@ -1,11 +1,1 @@
-name "metasploit-framework-extras"
-maintainer "Rapid7 Release Engineering <r7_re@rapid7.com>"
-homepage "https://rapid7.com"
 
-install_dir "#{default_root}/metasploit-framework-extras"
-
-build_version Omnibus::BuildVersion.semver
-build_iteration 1
-
-dependency "nmap"
-dependency "john"
