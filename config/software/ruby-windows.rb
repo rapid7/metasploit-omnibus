7b9fdb009933ef290d02cc691ae5b48241331b78
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2019-01-21 01:15:44 -0600

    update dependencies to ruby 2.5

--- config/software/ruby-windows.rb
+++ config/software/ruby-windows.rb
@@ -15,13 +15,16 @@
 #
 
 name "ruby-windows"
-default_version "2.4.3-2"
+default_version "2.5.3-1"
 
 if windows_arch_i386?
   relative_path "rubyinstaller-#{version}-x86"
 
   version "2.4.3-2" do
     source sha256: "ffd023d0aea50c3a9d7a4719c322aa46c4de54fdef55756264663ca74a7c13ea"
+  end
+  version "2.5.3-1" do
+    source sha256: "dc24e05c2c1490c74c6a7256c015cb786fb5f1898f2d8c92cbe4ca8fa271f24a"
   end
 
   source url: "https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-#{version}/rubyinstaller-#{version}-x86.7z"
@@ -30,6 +33,9 @@ else
 
   version "2.4.3-2" do
     source sha256: "3c9ace4e96a1bc7bca2c260bf35230e17662857e20a9637bade901cf55622661"
+  end
+  version "2.5.3-1" do
+    source sha256: "eabd682a6fb886a22168f568b9c508318f045dc2e130b2668e39c4a81d340ec9"
   end
 
   source url: "https://github.com/oneclick/rubyinstaller2/releases/download/rubyinstaller-#{version}/rubyinstaller-#{version}-x64.7z"
