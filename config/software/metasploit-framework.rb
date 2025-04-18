103451de40a7ae9f681d966e2d4577ce168139ee
adfoster-r7 <60357436+adfoster-r7@users.noreply.github.com>
2025-04-18 14:17:56 +0100

    Compile with Ruby 3.2.5 (#219)
    * Compile with Ruby 3.2.5
    
    * Remove fiddle for windows
    
    * Test windows artifacts

--- config/software/metasploit-framework.rb
+++ config/software/metasploit-framework.rb
@@ -51,6 +51,8 @@ whitelist_file "#{install_dir}//embedded/lib/ruby/gems/#{ruby_abi_version}/.*/sq
 
 build do
   patch_env = with_standard_compiler_flags(with_embedded_path)
+  # Remove problematic dependencies for Windows; Fiddle will need to be re-added in a future build for Ruby 3.3 support
+  patch source: "windows.patch", plevel: 0, env: patch_env if windows?
   copy "#{project_dir}", "#{install_dir}/embedded/framework"
 
   major, minor, patch = Omnibus::BuildVersion.semver.split('.')
--- config/software/pcaprub.rb
+++ config/software/pcaprub.rb
@@ -15,7 +15,7 @@
 #
 
 name "pcaprub"
-default_version "0.13.2"
+default_version "0.13.3"
 
 if windows?
   dependency "ruby-windows"
@@ -30,11 +30,26 @@ dependency "cacerts"
 dependency "rubygems"
 
 build do
+  # Skip pcaprub support for now as it causes compilation issues
+  next if windows?
+
   env = with_standard_compiler_flags(with_embedded_path)
   env['SSL_CERT_FILE'] = "#{install_dir}/embedded/ssl/cert.pem"
+  # Ignore:
+  # pcaprub.c: In function 'rbpcap_each_data':
+  # pcaprub.c:987:17: error: implicit declaration of function 'pcap_getevent'; did you mean 'pcap_geterr'? [-Wimplicit-function-declaration]
+  #   987 |   fno = (HANDLE)pcap_getevent(rbp->pd);
+  #       |                 ^~~~~~~~~~~~~
+  #       |                 pcap_geterr
+  # pcaprub.c:987:9: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
+  #   987 |   fno = (HANDLE)pcap_getevent(rbp->pd);
+  #       |         ^
+  # pcaprub.c: In function 'rbpcap_each_packet':
+  # pcaprub.c:1029:9: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
+  #  1029 |   fno = (HANDLE)pcap_getevent(rbp->pd);
+  cflags = "-Wno-int-to-pointer-cast -Wno-implicit-function-declaration"
 
-  # We need to create the pcaprub gem ourselves until 0.13.2 is released (https://github.com/pcaprub/pcaprub/issues/67)
-  # Use version of pcaprub relative to the current directory
+  # Use version of pcaprub relative to the current directory if it exists
   local_pcaprub_checkout = File.expand_path(File.join(Dir.pwd, "..", "pcaprub"))
   command "echo checking for path: #{local_pcaprub_checkout}", env: env
   if File.exist?(local_pcaprub_checkout)
@@ -43,6 +58,6 @@ build do
   else
     command "echo before old pcaprub code", env: env
     gem "install pcaprub" \
-      " --version '#{version}' --no-document", env: env
+      " --version '#{version}' --no-document -- --with-cflags=\'#{cflags}\'", env: env
   end
 end
